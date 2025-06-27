630735bcc727d69993729f607b29987360d3f396
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-06-27 15:08:41 +0200

    refactor logic and add tests for moving

--- testing/src/main/java/com/privatechef/PlayerCharacter.java
+++ testing/src/main/java/com/privatechef/PlayerCharacter.java
@@ -1,9 +1,46 @@
 package com.privatechef;
 
 public class PlayerCharacter {
+    int[] position = {0,0};
 
-
-    public static int getX(){
-        return 0;
+    public int getX() {
+        return this.position[0];
     }
+
+    public void setX(int x) {
+        this.position[0] = x;
+    }
+
+    public int getY() {
+        return this.position[1];
+    }
+
+    public void setY(int y) {
+        this.position[1] = y;
+    }
+
+    public int[] getPosition() {
+        return this.position;
+    }
+
+    public int[] moveW(){
+        position[0]++;
+        return this.position;
+    }
+
+    public int[] moveS(){
+        position[0]--;
+        return this.position;
+    }
+
+    public int[] moveA(){
+        position[1]--;
+        return position;
+    }
+
+    public int[] moveD(){
+        position[1]++;
+        return position;
+    }
+
 }
--- testing/src/test/java/com/privatechef/PlayerCharacterTest.java
+++ testing/src/test/java/com/privatechef/PlayerCharacterTest.java
@@ -1,14 +1,72 @@
 package com.privatechef;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerCharacterTest {
+    private PlayerCharacter player;
+
+    @Test
+    @BeforeEach
+    void setUp() {
+        player = new PlayerCharacter();
+        player.setX(0);
+        player.setY(0);
+    }
+
 
     @Test
     void getX_shouldReturn0(){
-        assertEquals(0, PlayerCharacter.getX());
+        assertEquals(0, player.getX());
+    }
+
+    @Test
+    void getY_shouldReturn0(){
+        assertEquals(0, player.getY());
+    }
+
+    @Test
+    void moveW_shouldReturn01(){
+        int[] expected = {1,0};
+        assertArrayEquals(expected, player.moveW());
+    }
+
+    @Test
+    void moveS_shouldReturn0minus1(){
+        int[] expected = {-1,0};
+        assertArrayEquals(expected, player.moveS());
+    }
+
+    @Test
+    void moveA_shouldReturnMinus10(){
+        int[] expected = {0,-1};
+        assertArrayEquals(expected, player.moveA());
+    }
+
+    @Test
+    void moveD_shouldReturn1and0(){
+        int[] expected = {0,1};
+        assertArrayEquals(expected, player.moveD());
+    }
+
+    @Test
+    void moveAround_shouldGoUpAndRight(){
+        int[] expected = {1,1};
+        player.moveW();
+        player.moveD();
+        assertArrayEquals(expected, player.getPosition());
+    }
+
+    @Test
+    void moveAround_shouldGoLeftLeftDownAndRight(){
+        int[] expected = {-1,-1};
+        player.moveA();
+        player.moveA();
+        player.moveS();
+        player.moveD();
+        assertArrayEquals(expected, player.getPosition());
     }
 
 }
