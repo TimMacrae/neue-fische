516f59bfa1a6d28797a9db01a3b73b017416cdbf
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-06-27 08:29:05 +0200

    add new maven package jfiglet

--- maven/src/main/java/com/privatechef/Main.java
+++ maven/src/main/java/com/privatechef/Main.java
@@ -1,11 +1,16 @@
 package com.privatechef;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int result = addition(5,5);
         System.out.println(result);
+
+        String asciiArt = FigletFont.convertOneLine("MAVEN BABY");
+        System.out.println(asciiArt);
     }
 
     public static int addition (int num, int num2){
