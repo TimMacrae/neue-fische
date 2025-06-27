e05de22cbb0d9b368da9f17d76f0a134a2673e2d
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-06-27 08:23:18 +0200

    compile and package with maven

---
+++ maven/src/main/java/com/privatechef/Main.java
@@ -0,0 +1,14 @@
+package com.privatechef;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int result = addition(5,5);
+        System.out.println(result);
+    }
+
+    public static int addition (int num, int num2){
+        return num+num2;
+    }
+}
