7f1f8c219e72bf0d942c4cfd0f6b419420c06a5d
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-01 08:36:39 +0200

    Add shell project

---
+++ shell-java/ShellJava.java
@@ -0,0 +1,9 @@
+public class ShellJava {
+    public static void main(String[] args) {
+        System.out.println("mkdir shell-java");
+	System.out.println("cd shell-java");
+	System.out.println("vi ShellJava.java");
+	System.out.println("javav ShellJava.java");
+	System.out.println("java ShellJava");
+    }
+}
