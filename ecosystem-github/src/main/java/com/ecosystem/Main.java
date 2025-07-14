a3556b64af8248dea4cdc9c3747a6d38fab5d9a0
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-14 10:19:14 +0200

    add master print

--- ecosystem-github/src/main/java/com/ecosystem/Main.java
+++ ecosystem-github/src/main/java/com/ecosystem/Main.java
@@ -4,7 +4,7 @@ package com.ecosystem;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("New branch");
+        System.out.println("Development branch");
     }
 
 
