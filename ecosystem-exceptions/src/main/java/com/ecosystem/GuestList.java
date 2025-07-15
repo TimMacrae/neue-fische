53bc66a4a9006b86ef5f2b2b191ddb878874496b
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-15 12:12:28 +0200

    add bonus tasks

--- ecosystem-exceptions/src/main/java/com/ecosystem/GuestList.java
+++ ecosystem-exceptions/src/main/java/com/ecosystem/GuestList.java
@@ -2,11 +2,51 @@ package com.ecosystem;
 
 import lombok.Data;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class GuestList {
-    private List<String> guestList = new ArrayList<>();
+
+    public GuestList(String path) throws IOException {
+        try {
+            Files.write(Paths.get(path), new ArrayList<>());
+        }catch (IOException e){
+            throw new IOException("Failed to initialize the guest list", e);
+        }
+    }
+
+    public List<String> setGuestList(List<String> guestList, String path) throws IOException {
+        try {
+            Files.write(Paths.get(path), guestList);
+        }catch (IOException e){
+            throw new IOException("Failed to write guest list to file", e);
+        }
+        return guestList;
+    }
+
+    public List<String> getGuestList(String path) throws IOException {
+        try {
+            return java.nio.file.Files.readAllLines(java.nio.file.Paths.get(path));
+        }catch (IIOException e){
+            throw new IOException("Failed to get the guest list to file", e);
+        }
+    }
+
+    public String addGuest(String guest, String path) throws IOException {
+        try {
+            List<String> guests = getGuestList(path);
+            guests.add(guest);
+            setGuestList(guests, path);
+        }catch (IIOException e){
+            throw new IOException("Failed to add the new guest to the list", e);
+        }
+        return guest;
+    }
 
 }
--- ecosystem-exceptions/src/test/java/com/ecosystem/GuestListTest.java
+++ ecosystem-exceptions/src/test/java/com/ecosystem/GuestListTest.java
@@ -3,30 +3,80 @@ package com.ecosystem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GuestListTest {
     GuestList guestList;
+    String path = "guests.txt";
+    String invalidPath = "/invalid/path/guests.txt";
 
     @BeforeEach
-    public void setup() {
-        guestList  = new GuestList();
+    public void setup() throws IOException {
+        guestList = new GuestList(path);
     }
 
     @Test
-    void guestList_ShouldBeEmpty_byDefault(){
-        assertEquals(0, guestList.getGuestList().size());
+    void guestList_ShouldBeEmpty_byDefault() throws IOException {
+        assertEquals(0, guestList.getGuestList(path).size());
     }
 
     @Test
-    void guestList_shouldReadSameGuestsAsWrittenBefore(){
+    void guestList_ShouldThrowAnIoException_whenInitializeWithWrongPath() {
+        assertThrows(IOException.class, () -> new GuestList(invalidPath)
+        );
+    }
+
+    @Test
+    void guestList_shouldReadSameGuestsAsWrittenBefore() throws IOException {
         List<String> newGuestList = new ArrayList<>(Set.of("Guest1", "Guest2", "Guest3"));
-        guestList.setGuestList(newGuestList);
-        assertEquals(3, guestList.getGuestList().size());
-        assertEquals(newGuestList, guestList.getGuestList());
+        List<String> textFileList = guestList.setGuestList(newGuestList, path);
+        assertEquals(3, textFileList.size());
+        assertEquals(newGuestList, textFileList);
+    }
+
+    @Test
+    void guestList_shouldWriteToFileSystem() throws IOException {
+        List<String> newGuestList = new ArrayList<>(Set.of("Theodor", "Anette"));
+        guestList.setGuestList(newGuestList, path);
+
+        assertEquals(newGuestList, guestList.getGuestList(path));
+    }
+
+    @Test
+    void setGuestList_shouldThrowIoException_whenFileWriteFails() {
+        List<String> guests = List.of("A", "B");
+
+        assertThrows(IOException.class, () -> {
+            guestList.setGuestList(guests, invalidPath);
+        });
+    }
+
+    @Test
+    void getGuestList_ShouldThrowIoException_whenFileReadFails() {
+        assertThrows(IOException.class, () -> guestList.getGuestList(invalidPath));
+    }
+
+    @Test
+    void addGuest_ShouldThrowIoException_whenFileWriteFails() {
+        assertThrows(IOException.class, () -> guestList.getGuestList(invalidPath));
+    }
+
+    @Test
+    void addGuest_ShouldAddedToTheList() throws IOException {
+        List<String> newGuestList = new ArrayList<>(Set.of("Guest1", "Guest2", "Guest3"));
+        List<String> textFileList = guestList.setGuestList(newGuestList, path);
+        assertEquals(3, textFileList.size());
+
+        List<String> updatedList = new ArrayList<>(Set.of("Guest1", "Guest2", "Guest3", "Tim"));
+        guestList.addGuest("Tim", path);
+        assertEquals(new HashSet<>(updatedList), new HashSet<>(guestList.getGuestList(path)));
+        assertEquals(4, guestList.getGuestList(path).size());
     }
 }
